2ab73ff16723b11235500a69727b2e28ec5f2b5a
Carlos Micó <carlosmico15@gmail.com>
2019-05-28 15:18:09 +0200

    Decrement and Remove buttons added

--- backend/index.js
+++ backend/index.js
@@ -1,42 +1,61 @@
-const express = require( 'express' );
+const express = require('express');
 const app = express();
-const mongoose = require( 'mongoose' );// importo mongoose como ODM 
-const CounterModel = require( './models/counter' );//importo modelo mongoose counter
-//conexión a la base de datos MongoDB CounterDB
-mongoose.connect( 'mongodb://localhost:27017/CounterDB', { useCreateIndex: true, useNewUrlParser: true } )
-    .then( () => console.log( "conexión establecida con éxito" ) )
-    .catch( err => console.log( "error al intentar conectar con mongodb " + err ) )
+const mongoose = require('mongoose'); // importo mongoose como ODM 
+const CounterModel = require('./models/counter'); //importo modelo mongoose counter
 
-app.use( function ( req, res, next ) {// para prevenir el cors error
-    res.header( "Access-Control-Allow-Origin", "*" );
-    res.header( "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept" );
-    res.header( 'Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS, PATCH' );
+//conexión a la base de datos MongoDB CounterDB (Si no encuentra la Base de Datos la crea el solo)
+mongoose.connect('mongodb://localhost:27017/CounterDB', { useCreateIndex: true, useNewUrlParser: true })
+    .then(() => console.log("conexión establecida con éxito"))
+    .catch(err => console.log("error al intentar conectar con mongodb " + err))
+
+app.use(function(req, res, next) { // para prevenir el cors error
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS, PATCH');
     next();
-} );
-app.get( '/all', ( req, res ) => {
-    CounterModel.find({}).then(counters=>res.send(counters))
-    .catch(err=>res.status(500).send(err))
+});
+
+//Endpoint que devuelve todos los counters de la BD
+app.get('/all', (req, res) => {
+    CounterModel.find({}).then(counters => res.send(counters))
+        .catch(err => res.status(500).send(err))
 })
 
+//Endpoint que añade un nuevo counter de la BD
+app.get('/addCounter/:count', (req, res) => {
+    count = req.params.count;
+    new CounterModel({
+            count,
+        }).save()
+        .then(counter => res.status(201).send(counter))
+        .catch(err => res.status(500).send(err));
+});
 
-app.get( '/addCounter/:count', ( req, res ) => {
-    count=req.params.count;
-    new CounterModel( {
-        count,
-    } ).save()
-    .then( counter => res.status(201).send(  counter ) )
-    .catch(err=>res.status(500).send(err));
-} );
-
-app.get('/increment/:id',(req,res)=>{
-    CounterModel.findById(req.params.id).then(counter=>{
-        counter.count=counter.count+1
-        CounterModel.findByIdAndUpdate(counter._id,{count:counter.count},{new:true,useFindAndModify:false})
-        .then(counter=>res.status(200).send(counter))
-    })
-    .catch(console.log)
+//Endpoint que incrementa el counter con el ID recibido por parametro de la URL
+app.get('/increment/:id', (req, res) => {
+    CounterModel.findById(req.params.id).then(counter => {
+            counter.count = counter.count + 1
+            CounterModel.findByIdAndUpdate(counter._id, { count: counter.count }, { new: true, useFindAndModify: false })
+                .then(counter => res.status(200).send(counter))
+        })
+        .catch(console.log)
 })
+
+//Endpoint que decrementa el counter con el ID recibido por parametro de la URL
+app.get('/decrement/:id', (req, res) => {
+    CounterModel.findById(req.params.id).then(counter => {
+            counter.count = counter.count - 1
+            CounterModel.findByIdAndUpdate(counter._id, { count: counter.count }, { new: true, useFindAndModify: false })
+                .then(counter => res.status(200).send(counter))
+        })
+        .catch(console.log)
+})
+
+//Endpoint que elimina el counter con el ID recibido por parametro de la URL
+app.get("/remove/:id", (req, res) => {
+    CounterModel.findByIdAndRemove(req.params.id).then((counter) => res.status(200).send({ "info": `Counter removed succesfully!`, counter })).catch();
+});
 
 //TODO ADD DECREMENT ENDPOINT 
 
-app.listen( 3001 )
+app.listen(3001)
--- frontend/main.js
+++ frontend/main.js
@@ -1,46 +1,68 @@
-document.addEventListener( 'DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
 
-    const addCounterToDOM=counterObject=>{
-      let counter=document.createElement('div');
-      counter.setAttribute("data-id",counterObject._id)
-      counter.className="counter"
-      counter.innerHTML=`
+    const addCounterToDOM = counterObject => {
+        let counter = document.createElement('div');
+        counter.setAttribute("data-id", counterObject._id)
+        counter.className = "counter"
+        counter.innerHTML = `
       <button class="decrementButton" >-</button>
       <span class="count" >${counterObject.count}</span>
-      <button class="incrementButton">+</button>`
-      document.querySelector(".Counters").appendChild(counter)
-      counter.querySelector(".incrementButton").addEventListener('click',event=>{
-        fetch(`http://localhost:3001/increment/${counterObject._id}`)
-        .then(res=>res.json())
-        .then(res=>{
-         document.querySelector(`[data-id="${res._id}"] span`).innerHTML=res.count
+      <button class="incrementButton">+</button>
+      <button class="removeButton">🗑️</button> 
+      `
+        document.querySelector(".Counters").appendChild(counter)
+
+        //Buttons EVENTS
+        counter.querySelector(".incrementButton").addEventListener('click', event => {
+            fetch(`http://localhost:3001/increment/${counterObject._id}`)
+                .then(res => res.json())
+                .then(res => {
+                    document.querySelector(`[data-id="${res._id}"] span`).innerHTML = res.count
+                })
+                .catch(console.log)
+        })
+
+        counter.querySelector(".decrementButton").addEventListener('click', event => {
+            fetch(`http://localhost:3001/decrement/${counterObject._id}`)
+                .then(res => res.json())
+                .then(res => {
+                    document.querySelector(`[data-id="${res._id}"] span`).innerHTML = res.count
+                })
+                .catch(console.log)
+        })
+
+        counter.querySelector(".removeButton").addEventListener('click', event => {
+            fetch(`http://localhost:3001/remove/${counterObject._id}`)
+                .then(res => res.json())
+                .then(res => {
+                    console.log(res)
+                    counter.remove();
+                })
+                .catch(console.log)
+        })
+    }
+    fetch(`http://localhost:3001/all`)
+        .then(res => res.json())
+        .then(res => {
+            for (const counter of res) {
+                addCounterToDOM(counter)
+            }
         })
         .catch(console.log)
-      })
-      //TODO ADD DECREMENT BUTTON LISTENER
-    }
-    fetch( `http://localhost:3001/all` )
-        .then( res => res.json() )
-        .then( res => {
-            for (const counter of res) {
-              addCounterToDOM(counter)
-            }
-        } )
-        .catch(console.log)
 
-    document.querySelector( '.inputCounter' ).addEventListener( 'keyup', event => {
-        if ( event.keyCode === 13 ) {
-            if ( event.target.value === "" ) {
+    document.querySelector('.inputCounter').addEventListener('keyup', event => {
+        if (event.keyCode === 13) {
+            if (event.target.value === "") {
                 event.target.value = 0;
             }
-            fetch( `http://localhost:3001/addCounter/${event.target.value}` )
-                .then( res => res.json() ).then( res => {
-                  addCounterToDOM(res)
-                } )
+            fetch(`http://localhost:3001/addCounter/${event.target.value}`)
+                .then(res => res.json()).then(res => {
+                    addCounterToDOM(res)
+                })
                 .catch(console.log)
 
             event.target.value = ""
 
         }
-    } )
-} );
+    })
+});
